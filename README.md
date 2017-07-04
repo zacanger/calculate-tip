f60ba7eb3e3cb08bbf6df29ac71c53ce44bb9ada
zacanger <zac@zacanger.com>
2017-07-03 18:31:03 -0600

    Here's a thing.

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
 # calculate-tip
 
+Tiny command-line tip calculator.
+
 --------
 
 ## Installation:
 
+`npm i -g calculate-tip`
+
 ## Usage:
+
+`calculate-tip price percentage`
+
+`percentage` defaults to `20`.
+
+Examples:
+
+`calculate-tip 39.99`
+
+`calculate-tip 39.99 17`
+
+## FAQ
+
+* Why?
+  * I'm really bad at doing math in my head
 
 ## License:
 
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 
-const main = () => {
+// usage:
+// ./tip-calculator.js price percentage
+// percentage defaults to 20
 
-}
+const cost = Number(process.argv[2])
+const perc = Number(process.argv[3] || 20)
+const tip = cost * (perc / 100)
 
-if (!module.parent) main()
+console.log(`
+  Tip: ${tip.toFixed(2)}
+  Total: ${(tip + cost).toFixed(2)}
+`)
