72a335dc84926086b363a6b80bf7a6ca76976bf3
zacanger <zac@zacanger.com>
2020-09-25 16:44:35 -0600

    chore: ts

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
 #!/usr/bin/env node
 
-if (!process.argv[2]) {
-  console.log(`
-    Usage:
-    calculate-tip price percentage
-    percentage defaults to 20
-    Examples:
-    calculate-tip 39.99
-    calculate-tip 39.99 17
-`)
-  process.exit(0)
-}
-
-const cost = Number(process.argv[2])
-const perc = Number(process.argv[3] || 20)
-const tip = cost * (perc / 100)
-
-console.log(`
-  Tip: ${tip.toFixed(2)}
-  Total: ${(tip + cost).toFixed(2)}
-`)
+require('./lib')
